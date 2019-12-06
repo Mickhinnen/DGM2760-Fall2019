5518fc56e5621d046a47ae99a0aabbb29e75f5aa
Mickey <mickailahinnen@gmail.com>
2019-12-06 12:28:35 -0700

    now it looks nice

--- assignment06/js/main.js
+++ assignment06/js/main.js
@@ -16,7 +16,7 @@ function tellStory() {
     const myStory = `Once upon a time there were four ${adjList[0]} ${nounList[0]}s named Floppsy, Mopsy. Cotton-tail and Petter.
     They lived with their Mother underneath a ${nounList[1]} near a ${adjList[1]} ${nounList[2]}.
     Now, my dears, said ${adjList[2]} Mrs.Rabbit, you may go over the ${nounList[3]} or around the ${nounList[4]}, 
-    but do not go into Mr. McGrehor's ${nounList[5]},
+    but do not go into Mr. McGregor's ${nounList[5]},
     your Father had an accident there and he was put in a ${nounList[6]} by Mr. McGregor. 
     Now ${verbList[0]} along, and don't get into mischeif. I am going ${verbList[1]}ing`
 
