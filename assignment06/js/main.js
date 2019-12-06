57f0d0800da71eacad074513a16592bd5fcaaf7f
Mickey <mickailahinnen@gmail.com>
2019-12-06 00:38:16 -0700

    almost there, new text doesn't work yet

--- assignment06/js/main.js
+++ assignment06/js/main.js
@@ -1,13 +1,19 @@
 
 function tellStory() {
-    const nounArray = document.querySelector('#nouns').textContent   
-    nounArray.toString().split(/\s+/)
-   // const adjectiveArray = 
+    
+    const nounArray = document.querySelector('#nouns').textContent
+    //const nounLower = nounArray.toLowerCase()
+    const nounList = nounArray.toLowerCase().split(/\s+/)
+    
+    
+    const adjectiveArray = document.querySelector('#adjectives').textContent
+    const adjList = adjectiveArray.toLowerCase().split(/\s+/)
 
-   // const verbArray = 
+    const verbArray = document.querySelector('#verbs').textContent
+    const verbList = verbArray.toLowerCase().split(/\s+/)
 
-    const myStory = `Once upon a time there were four ${nounArray[0]}s `
+    const myStory = `Once upon a time there were four ${nounList[1]}s, ${adjList[1]}s, ${verbList[1]} `
 
     document.querySelector('#story').innerText = myStory
-    console.log(myStory)
+    
 }
