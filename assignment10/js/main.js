de3c69f4cbac8025d7e9265c829be9292a97e184
Mickey <mickailahinnen@gmail.com>
2019-12-06 23:38:06 -0700

    little things

--- assignment10/js/main.js
+++ assignment10/js/main.js
@@ -1,6 +1,6 @@
 function duplicateMenu() {
   // get all of the anchor elements from the top menu // <a> is anchor
- let topList = document.querySelectorAll('ul#primaryNavigation li a') //tech a node list has array like behavior
+ let topList = document.querySelectorAll('ul#primaryNavigation li a') //technically a node list has array like behavior
 
  // create the new list items for the bottom of page
  let newList = document.createElement('ul')
