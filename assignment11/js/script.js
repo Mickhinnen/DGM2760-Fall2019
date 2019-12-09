e72d6fab2604f3fcdedb20ae7ee8f06ebcf6b852
Mickey <mickailahinnen@gmail.com>
2019-12-08 21:04:11 -0700

    videos watched

--- assignment11/js/script.js
+++ assignment11/js/script.js
@@ -3,7 +3,7 @@
 async function getHotelData() {
  try {
     const reponse = await fetch ('../assignment11/hotel.json')
-    return await response.json() //return json object
+    return await reponse.json //return json object
  }   catch (error) {
      console.error(error)
  }
@@ -11,4 +11,20 @@ async function getHotelData() {
 
 let hotelData = {}
 getHotelData().then(data => hotelData = data)
-console.log(hotelData)
+//console.log(hotelData)
+
+// store in a variable document.querySelectorAll('#a')
+// use that variable to loop over ech element and addEventlistener to each one
+// sugested use of forEach
+document.querySelector('#marriott').addEventListener('click', hotelInfo)
+
+function hotelInfo(event) {
+   let hotelChoice = hotelData.hotels.find(hotel => {
+       return event.target.id === hotel.name.toLowerCase()
+   })
+    console.log(hotelChoice)
+  //  console.log(event.target.id)
+
+   document.querySelector('#hotelName').textContent = `${hotelChoice.name} Hotel`
+
+}
